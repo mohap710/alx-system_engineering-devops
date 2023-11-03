703ec0c93f80a3a2d5166975a62b289cffca238f
mohap710 <mohap710@gmail.com>
2023-11-03 13:47:32 +0200

    deferment to month 4

--- 0x0F-load_balancer/README.md
+++
@@ -1,1 +0,0 @@
-# How Load balancers works
--- 0x14-mysql/1-create_user.sql
+++
@@ -1,4 +0,0 @@
--- lets create a user on the database servers
-CREATE USER IF NOT EXISTS 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
-GRANT REPLICATION CLIENT, REPLICATION SLAVE ON *.* TO 'holberton_user'@'localhost';
-FLUSH PRIVILEGES;
