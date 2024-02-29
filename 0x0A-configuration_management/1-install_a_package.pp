9d2879259adcdbaf2fa1d204e54f6a82058fffe1
mohap710 <mohap710@gmail.com>
2024-02-29 18:27:55 +0200

    Finally pip install flask done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,5 @@
-# Explicitly declare dependency on the standard package resource type
-require Stdlib::Package
-
-# Specify the pip provider for package management
-$config['package']['default_provider'] = 'pip'
-
-# Install Flask version 2.1.0
-package { 'flask':
-    ensure  => installed,
-    version => '2.1.0',
-}
+# install flask from pip3
+package { 'flask' :
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    }
