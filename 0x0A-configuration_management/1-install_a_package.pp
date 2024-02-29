9cb9ae28cefe6c43ad571a8e0c393d7f88f42203
mohap710 <mohap710@gmail.com>
2024-02-29 17:49:18 +0200

    install flask using pip

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,10 @@
-# install flask -v 2.1.0
+# Ensure pip provider is enabled
+$config['package']['default_provider'] = 'pip'
 
-exec { 'flask':
-  command => '/usr/bin/pip install flask -v 2.1.0',
+# Install Flask version 2.1.0
+package { 'flask':
+    ensure => installed,
+    provider => 'pip',
+    require => Class['package'],
+    version => '2.1.0',
 }
